fddb7fb8576a9b5f5cf4b8343dad8beddbf76dc4
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-08-21 13:24:53 +0600

    Install tailwind CSS

---
+++ Client/src/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Signup = () => {
+    return (
+        <div>
+            <h1 className='text-2xl text-amber-300'>Sign UP</h1>
+        </div>
+    );
+};
+
+export default Signup;

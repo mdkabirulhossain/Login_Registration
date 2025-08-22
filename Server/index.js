625245fa1d271882dad239da343f8f0823215005
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-08-22 12:11:35 +0600

    Connect Database

--- Server/index.js
+++ Server/index.js
@@ -1,15 +1,26 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'
+import mongoose from 'mongoose';
 
 const app = express();
 
+//Middleware
+app.use(express.json())
 app.use(cors());
 
 
 const PORT = process.env.PORT || 5000 ;
 
 // console.log("HI", PORT);
+//Database Connection
+URL=process.env.Database_Url
+
+mongoose.connect(URL).then(()=>{
+    console.log('Connected to mongoDB');
+}).catch((error)=>{
+    console.log(error);
+})
 
 app.get('/', (req, res) =>{
     res.send("Hello");
